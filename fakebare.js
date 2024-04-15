8a452df2b13fa61433fa88bc96d3122116ac6b62
UBG Ultra <155318917+ubgultra@users.noreply.github.com>
2024-04-15 07:02:44 -0400

    Create fakebare.js

---
+++ fakebare.js
@@ -0,0 +1,15 @@
+{
+	"versions": [
+		"v1",
+		"v2",
+		"v3"
+	],
+	"language": "NodeJS",
+	"memoryUsage": 26.53,
+	"project": {
+		"name": "bare-server-node",
+		"description": "TOMPHTTP NodeJS Bare Server",
+		"repository": "sadamjhgf",
+		"version": "2.0.2"
+	}
+}
