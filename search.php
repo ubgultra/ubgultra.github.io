c3f37ef3e0cef6592e349ee2ac2c4dae4afba645
UBG Ultra <155318917+ubgultra@users.noreply.github.com>
2024-06-20 18:35:11 -0400

    Create search.php

---
+++ search.php
@@ -0,0 +1,7 @@
+$search = $_POST['findMe'];
+//$search = "example";//THIS WOULD WORK, BUT I WAS SHOWING HOW TO USE FORM
+
+//IF WORD FOUND IN HOME PAGE
+if (stripos($home, $search) !== false) {//USING EXAMPLE.COM TO SHOW IT WORKS
+    echo '<a href="'.$homePageName.'">'.$homePageName.'</a>';
+}
